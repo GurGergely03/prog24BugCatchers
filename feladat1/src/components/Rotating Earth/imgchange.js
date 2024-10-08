60778d889bacd0f7a4b5dd7496a1079f6db89adf
TheAshenWitch <jaholamark410@gmail.com>
2024-10-08 13:26:48 +0200

    something

---
+++ feladat1/src/components/Rotating Earth/imgchange.js
@@ -0,0 +1,30 @@
+// new image
+var img = new Image();
+
+//add text
+img.onload = () => {
+    let canvas = document.createElement("canvas"),
+    ctx = canvas.getContext("2d");
+
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    ctx.drawImage(img, 0 ,0, img.naturalWidth, img.naturalHeight);
+
+    //text settings
+    let txt = "MUKODIIK", x=30, y= 220;
+    ctx.font = "bold 48px Arial";
+    ctx.fillStyle = "rgb(255,255,255)";
+    ctx.fillText(txt,x,y);
+    let canvasUrl = canvas.toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.save = "download-this-canvas";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+}
+img.src = "earth.jpg"
