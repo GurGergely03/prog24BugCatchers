5b5cce2840708b9bc1d16d4f52301347404b406a
TheAshenWitch <jaholamark410@gmail.com>
2024-10-08 14:31:31 +0200

    a

--- Rotating Earth/imgchange.js
+++ Rotating Earth/imgchange.js
@@ -15,16 +15,17 @@ img.onload = () => {
     ctx.font = "bold 48px Arial";
     ctx.fillStyle = "rgb(255,255,255)";
     ctx.fillText(txt,x,y);
-    let canvasUrl = canvas.toDataURL();
+    let canvasUrl = canvas.toDataURL("image/jpeg");
     // Create an anchor, and set the href value to our data URL
     const createEl = document.createElement('a');
     createEl.href = canvasUrl;
 
     // This is the name of our downloaded file
-    createEl.save = "download-this-canvas";
-
+    createEl.download = "newearth";
+    createEl.hidden = true;
     // Click the download button, causing a download, and then remove it
     createEl.click();
     createEl.remove();
 }
 img.src = "earth.jpg"
+
